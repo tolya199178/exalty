f842741cda7717b7aa951cba890355a2e11a2ca4
Travis <travis@travis-ci.org>
2016-04-13 00:45:23 +0800

    check payment card

--- app/components/crediticard-form.js
+++ app/components/crediticard-form.js
@@ -7,19 +7,34 @@ export default Ember.Component.extend(EmberValidations, {
 	cardNumber:null,
   	expireDate:null,
   	cvc:null,
+  	requiredcard:false,
+  	accouneEdit:true,
+  	loadding:false,
   	didInsertElement() {
   		let that = this;
 		this._super();
-		$(this.$()).parents(".modal-dialog").attr("style", "max-width:500px;margin-top:55px")
+		$(this.$()).parents(".modal-dialog").attr("style", "max-width:500px;margin-top:55px");
 		$(".expiredate input", this.$()).mask("99/99");		
+		if(this.get("requiredcard") == true){			
+			$("body").addClass("fademodal");
+			this.set("accouneEdit", false);
+		}else{
+			this.set("accouneEdit", true);
+		}
+
 		let userId = this.get("authorize").getUserId();
 		let cardsRef = new Firebase(config.firebase + "/customers/" + userId + "/sources/data");
+		let card = null
 		cardsRef.on("value", function(snapshot) {
+			if(card){
+				return true;
+			}
 		    let data = snapshot.val();
 		    if(data && data.length){
 		    	let length = data.length;
 		   		let row = data[length-1];
-		   		let last4 = row.last4;
+		   		card = row;
+		   		let last4 = row.last4;		   		
 		   		that.set("cardNumber", "************" + last4);
 				that.set("expireDate", "**/**");
 				that.set("cvc", "***");
@@ -57,22 +72,24 @@ export default Ember.Component.extend(EmberValidations, {
 			let cvc =  this.get("cvc");
 			let date = expireDate.split('/');		
 			
-			Stripe.setPublishableKey(config.stripePublishableKey);				
-
+			that.set("loadding", true);
+			Stripe.setPublishableKey(config.stripePublishableKey);
 			Stripe.card.createToken({			      
 		      'number':cardNumber,
               'cvc':cvc,
               'exp-month':date[0],
               'exp-year':date[1],
 		    }, function(status, response) {
+		    	that.set("loadding", false);
 			      if (response.error) {
 			        that.get("showNotification")(response.error.message, "error"); 
-			      } else {
+			      } else {			      	
 			        var token = response['id'];
 			        var cardsRef = new Firebase(config.firebase + "/cards");
 					cardsRef.child(that.get("authorize").getUserId()).set({
 						card:token
-					});	
+					});
+					$("body").removeClass("fademodal");	
 					that.get("showNotification")("Your credit card changed successfully", "success");
 					Ember.run.later((function(){
 						that.get("modalclose")();
--- app/controllers/app.js
+++ app/controllers/app.js
@@ -7,14 +7,74 @@ export default Ember.Controller.extend({
 	eventwizard:Ember.inject.service(),
 	routing: Ember.inject.service(),
 	eventService: Ember.inject.service(),
+	authorize:Ember.inject.service(),
 	eventData:[],
 	loading:false,
+
+	crediticardModalView:false,
+
 	init(){
 		let that = this;
 		this.get("eventService").initObj();
 		this.get("eventService").addObserver("eventCalendarData", function(evnets){
 			that.set("eventData", that.get("eventService").get("eventCalendarData"));
 		});
+
+		/**
+		 * Check Payment
+		 */
+		let userId = this.get("authorize").getUserId();
+		let planRef = new Firebase(config.firebase + "/customers/" + userId + "/subscriptions/data");
+		let cardsRef = new Firebase(config.firebase + "/customers/" + userId + "/sources/data");
+		let plan = null;
+		let card = null;
+		planRef.on("value", function(snapshot) {
+			if(plan){
+				return true;
+			}
+		    let data = snapshot.val();
+		    if(data && data.length){
+		    	plan = data[data.length-1];
+		    	cardsRef.on("value", function(snapshot) {
+		    		if(card){
+		    			return true;
+		    		}
+				    let data = snapshot.val();
+				    if(data && data.length){
+				    	card = data[data.length-1];
+				    }else{
+
+				    }
+				    let trialEnd = moment(plan.current_period_end*1000);
+
+				    that.get("requirePayment")(that);
+
+				    if(moment().diff(trialEnd) > 0){
+				    	/*ended trial period*/
+				    	if(!card 
+				    		|| (card.exp_year < moment().format("Y")) 
+				    		|| (card.exp_year == moment().format("Y") && card.exp_month < moment().format("M"))){
+				    		that.get("requirePayment")(that);
+				    	}
+				    }
+				    /**
+				     * Check Period
+				     */
+
+				}, function (errorObject) {
+				  console.log("The read failed: " + errorObject.code);
+				});
+		    }else{
+		    	let user = this.get("authorize").getUser();
+		    	user.set("addedSubscription", false);
+		    	user.save();
+		    }
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
+		});
+	},
+	requirePayment(that){
+		that.set("crediticardModalView", true);
 	},
 	actions: {
 		clicked(event, jsEvent, view){
@@ -52,6 +112,12 @@ export default Ember.Controller.extend({
 			let ref = new Firebase(config.firebase);
   			ref.unauth();
 			this.get("routing").transitionTo('landing');
+		},
+		showNotification(message, type){
+			this.get("applicationController").send("showNotification" ,message, type);
+		},
+		creditiCardModalClose(){
+			this.set("crediticardModalView", false);
 		}
 	}
 });
